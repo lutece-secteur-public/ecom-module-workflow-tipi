c65c20f74f13c5bdc0e386aef28b893bc97a8591
MOIRE <cyril.moire@atos.net>
2018-06-26 18:23:27 +0200

    license

--- src/java/fr/paris/lutece/plugins/workflow/modules/tipi/web/task/TipiTaskComponent.java
+++ src/java/fr/paris/lutece/plugins/workflow/modules/tipi/web/task/TipiTaskComponent.java
@@ -28,7 +28,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * License 1.0
  */
 package fr.paris.lutece.plugins.workflow.modules.tipi.web.task;
